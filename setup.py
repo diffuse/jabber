b20a6abefbc57eff0dbd76f6744a286da60328fa
Diffuse <48339639+diffuse@users.noreply.github.com>
2020-04-29 20:51:38 -0400

    Add basic setup file and directory structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='jabber',
+   version='0.0.0',
+   description='',
+   author='',
+   author_email='',
+   packages=find_packages(),  
+   test_suite='tests'
+)
