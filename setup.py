a69eb05a73ba3febe110baf40f7b772ea03effbf
Diffuse <48339639+diffuse@users.noreply.github.com>
2020-05-09 15:35:30 -0400

    Update description and author name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
    name='jabber',
    version='0.0.0',
-   description='',
-   author='',
+   description='Label single or multiclass data with minimal keystrokes',
+   author='Diffuse',
    author_email='',
    packages=find_packages(),  
    test_suite='tests'
