8cbca362682ad88c9075bcc191f0759c0370ecaa
Diffuse <48339639+diffuse@users.noreply.github.com>
2020-05-09 12:20:27 -0400

    Remove mic UI

--- jabber/gui/__init__.py
+++ jabber/gui/__init__.py
@@ -4,5 +4,4 @@ ui_path = 'jabber/gui/ui'
 ClassListForm, ClassListBase = uic.loadUiType(f'{ui_path}/class_list.ui')
 ImgForm, ImgBase = uic.loadUiType(f'{ui_path}/image.ui')
 ImgListForm, ImgListBase = uic.loadUiType(f'{ui_path}/image_list.ui')
-MicForm, MicBase = uic.loadUiType(f'{ui_path}/mic.ui')
 MWForm, MWBase = uic.loadUiType(f'{ui_path}/main_window.ui')
