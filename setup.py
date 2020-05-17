03abd5dced6ae62dcc7ae8082b6b79d5a29ffa6a
Diffuse <48339639+diffuse@users.noreply.github.com>
2020-05-16 21:56:38 -0400

    Reduce excessive font size

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='jabber',
-   version='0.1.1',
+   version='0.1.2',
    description='Label single or multiclass data with minimal keystrokes',
    author='Diffuse',
    author_email='',
